ce59c5d17bd79654da0022b2dbe820900591f9f3
Angel Peñaflor <aepenaflor@proton.me>
2025-04-09 02:05:03 -0600

    Initial commit: Server is capable listening on port 8080, accept incoming connection and seting a basic HTTP response back to the client.

---
+++ Makefile
@@ -0,0 +1,26 @@
+# Compiler and flags
+CC      = clang
+CFLAGS  = -std=c23 -Weverything -Wpedantic -O0 -Werror
+
+# Directories and target
+SRC_DIR    = src
+BUILD_DIR  = build
+TARGET     = $(BUILD_DIR)/server
+
+# Default target
+all: $(TARGET)
+
+# Build the target executable from server.c
+$(TARGET): $(SRC_DIR)/server.c | $(BUILD_DIR)
+	$(CC) $(CFLAGS) -o $(TARGET) $(SRC_DIR)/server.c
+
+# Create the build directory if it doesn't exist
+$(BUILD_DIR):
+	mkdir -p $(BUILD_DIR)
+
+# Clean up generated files
+clean:
+	rm -rf $(BUILD_DIR)
+
+# Phony targets
+.PHONY: all clean
---
+++ src/server.c
@@ -0,0 +1,68 @@
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT    8080
+#define BACKLOG 16
+
+int main() {
+    int                TRUE = 1;
+    int                server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    unsigned int       client_addr_size = sizeof(client_addr);
+    const char        *OK_RESPONSE      = "HTTP/1.1 200 OK\r\n\r\n";
+
+    // 1. Create a socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation faild!");
+        exit(EXIT_FAILURE);
+    }
+
+    // 2. Set socket options.
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &TRUE, sizeof(TRUE)) < 0) {
+        perror("setsockopt failed !");
+        exit(EXIT_FAILURE);
+    }
+
+    // 3. Bind the socket to an addr (Any available) and port (8080)
+    // The server_addr stores an IPv4 addr and port
+    server_addr.sin_family      = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port        = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
+        perror("Binding failed!");
+        exit(EXIT_FAILURE);
+    }
+
+    // 4. Listen on the socket for incoming connections
+    if (listen(server_fd, BACKLOG) != 0) {
+        perror("Listeing failed!");
+        exit(EXIT_FAILURE);
+    }
+
+    // 5. Accept incoming connection
+    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_size);
+    if (client_fd == -1) {
+        perror("Accepting incoming connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // 6. Send response to the client
+    if ((send(client_fd, OK_RESPONSE, strlen(OK_RESPONSE), 0)) == -1) {
+        perror("Sending response failed!");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("%s", OK_RESPONSE);
+    }
+
+    shutdown(client_fd, SHUT_WR);
+    close(client_fd);
+    close(server_fd);
+
+    return EXIT_SUCCESS;
+}
