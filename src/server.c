4b935d7346ca50fde5b7bf7ccba5c02b866317a5
Angel Peñaflor <aepenaflor@proton.me>
2025-04-10 00:45:50 -0600

    Server is now able to return 200 status code for index.html and 404 status for everything else

--- src/server.c
+++ src/server.c
@@ -6,15 +6,22 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT    8080
-#define BACKLOG 16
+#define PORT             8080
+#define BACKLOG          16
+#define MAX_REQUEST_SIZE 1024
 
 int main() {
-    int                TRUE = 1;
-    int                server_fd, client_fd;
+    const char *OK_RESPONSE = "HTTP/1.1 200 OK\r\n\r\n";
+    const char *NOT_FOUND   = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+    int TRUE = 1;
+
+    int                server_fd, client_fd = -1;
     struct sockaddr_in server_addr, client_addr;
     unsigned int       client_addr_size = sizeof(client_addr);
-    const char        *OK_RESPONSE      = "HTTP/1.1 200 OK\r\n\r\n";
+
+    ssize_t bytes_read;
+    char   *request_buffer = calloc(MAX_REQUEST_SIZE * sizeof(char), sizeof(char));
 
     // 1. Create a socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
@@ -45,23 +52,40 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // 5. Accept incoming connection
-    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_size);
-    if (client_fd == -1) {
-        perror("Accepting incoming connection failed");
-        exit(EXIT_FAILURE);
-    }
+    while (TRUE) {
+        // 5. Accept incoming connection
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_size);
+        if (client_fd == -1) {
+            perror("Accepting incoming connection failed");
+            exit(EXIT_FAILURE);
+        }
 
-    // 6. Send response to the client
-    if ((send(client_fd, OK_RESPONSE, strlen(OK_RESPONSE), 0)) == -1) {
-        perror("Sending response failed!");
-        exit(EXIT_FAILURE);
-    } else {
-        printf("%s", OK_RESPONSE);
-    }
+        // 6. Read and print client's request
+        bytes_read = recv(client_fd, request_buffer, MAX_REQUEST_SIZE, 0);
+        if (bytes_read > 0) {
+            request_buffer[bytes_read] = '\0';
+        } else {
+            perror("recv failed!");
+        }
+        printf("bytes_read: %ld\n", bytes_read);
+        printf("request_buffer:\n---\n%s\n---\n", request_buffer);
 
-    shutdown(client_fd, SHUT_WR);
-    close(client_fd);
+        // 7. Send response to the client
+        if (strstr(request_buffer, "GET /index.html HTTP/1.1\r\n") != NULL) {
+            if ((send(client_fd, OK_RESPONSE, strlen(OK_RESPONSE), 0)) == -1) {
+                perror("Sending response failed!");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            if ((send(client_fd, NOT_FOUND, strlen(NOT_FOUND), 0)) == -1) {
+                perror("Sending response failed!");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        shutdown(client_fd, SHUT_WR);
+        close(client_fd);
+    }
     close(server_fd);
 
     return EXIT_SUCCESS;
